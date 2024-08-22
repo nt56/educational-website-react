2d093091148489f1de80588f136ed46efd04e2c0
nt_56 <nagutirth2121@gmail.com>
2024-08-22 11:03:20 +0530

    setup the project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+
+      <ul></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
