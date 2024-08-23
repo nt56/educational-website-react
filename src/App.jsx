5b4f2ccfc84e3a6a4a77fa105dbc297688ac2359
nt_56 <nagutirth2121@gmail.com>
2024-08-23 09:21:36 +0530

    upto about completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our Program" title="What We Offer" />
+        <Programs />
+        <About />
+        <Title subTitle="Gallwry" title="Campus Photos" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,31 @@
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Navbar = () => {
-  return (
-    <nav>
-      <img src={logo} alt="logo" />
+  const [sticky, setSticky] = useState(false);
 
-      <ul></ul>
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
+
+  return (
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+      <img src={logo} alt="logo" className="logo" />
+
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About</li>
+        <li>Campus</li>
+        <li>Testomonials</li>
+        <li>
+          <button className="btn">Contact</button>
+        </li>
+      </ul>
     </nav>
   );
 };
